d530589849cc22ebfb8e15345ce014e4af27d0e2
Rishant Dwivedi <Rishant Dwivedi>
2016-03-11 17:51:16 -0600

    Illness Route

---
+++ app/routes/illness.js
@@ -0,0 +1,46 @@
+var Illness = require('../models/Illness');
+
+module.exports = {
+	addIllness: function (req, res) {
+		var body = req.body;
+
+		var newIllness = new Illness({
+			name: body.name,
+			symptoms: body.symptoms,
+			createdAt: body.timestamp 
+		});
+
+		newIllness.save(function(err, newIll){
+			//Adds the Illness to the database
+			console.log("Error" + err);
+			if (err) {
+				return res.status(400).send({message: "Illness Not Added"});
+			} else {
+				return res.status(200).send({message: "Illness Added", illness: newIll});
+			}
+		});
+	},
+	getIllness: function (req, res) {
+		//Gets the Illness for a particular search from the database
+		var body = req.body;
+
+		Illness.find({name: body.name}, function( err, illness) {
+			if (err) {
+				return res.status(400).send({message: "Illness Not Found", data: []});
+			} else {
+				return res.status(200).send({message: "Illness Found", data: illness});
+			}
+		});
+	},
+	deleteIllness: function(req, res) {
+		var body = req.body;
+
+		Illness.remove({name: body.name},function( err, response) {
+			if (err) {
+				return res.status(400).send({message: "Illness Not Found"});
+			} else {
+				return res.status(200).send({message: "Illness Deleted"});
+			}
+		});
+	}
+};
