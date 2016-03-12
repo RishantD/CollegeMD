e60cab839e30948f4e1e7162c1ac62d9288a58c6
Vaibhav Sharma <vibhu1201@gmail.com>
2016-03-11 21:59:17 -0600

    Pushing before first test

--- public/main_controller.js
+++ public/main_controller.js
@@ -2,12 +2,12 @@
 	var app = angular.module('collegemd');
 	app.controller('main_controller', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
 		
-		$scope.illness = "";
+		$scope.illness = '';
 
 		$scope.searchIllness = function() {
 			$http({
   				method: 'GET',
-  				url: '/someUrl',
+  				url: '/api/Illness/get',
   				data: $.param($scope.illness)
 			});
 		};
